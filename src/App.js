91d19f7f3eb27742d02071fcf991e6b98b9ec440
Jack White <jfw793@gmail.com>
2021-12-26 21:09:04 +1100

    Fixed typo

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   
   // <BrowserRouter> works fine locally, but HashRouter needed for deployment. Doesn't work rn.
   return (
-    <HashRouter basename='/ecommerce-app'>
+    <HashRouter>
       <Routes>
         <Route exact path='/' element={<Home cart={cart}/>}/>
         <Route exact path='/products' element={<Products 
