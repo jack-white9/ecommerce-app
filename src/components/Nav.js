8c3f073551f4ef02c3fbd8ddde3a3dde53179f4b
Jack White <jfw793@gmail.com>
2021-12-27 10:46:03 +1100

    Updated Nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,11 +7,11 @@ const Nav = (props) => {
     return (
         <nav>
             <ul className="navbar">
-                <Link to="/" className="navbar__logo">Eco-Store</Link>
+                <Link to="/ecommerce-app" className="navbar__logo">Eco-Store</Link>
                 <div className="navbar__links">
-                    <li><a href="/" className="navbar__links--link navbar__home">Home</a></li>
-                    <li><a href="/products" className="navbar__links--link navbar__products">Products</a></li>
-                    <li><a href="/cart" className="navbar__links--link"><FontAwesomeIcon icon={faShoppingCart}/>&nbsp;({props.cart ? props.cart.length : 0})</a></li>
+                    <li><a href="/ecommerce-app" className="navbar__links--link navbar__home">Home</a></li>
+                    <li><a href="/ecommerce-app/products" className="navbar__links--link navbar__products">Products</a></li>
+                    <li><a href="/ecommerce-app/cart" className="navbar__links--link"><FontAwesomeIcon icon={faShoppingCart}/>&nbsp;({props.cart ? props.cart.length : 0})</a></li>
                 </div>
             </ul>
         </nav>
