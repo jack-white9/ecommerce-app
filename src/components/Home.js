fda5893df07831d0552a1738f0a6ab93a3e859ed
Jack White <jfw793@gmail.com>
2021-12-26 15:51:03 +1100

    Added basic home styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Nav from './Nav'
+import World from '../assets/world.svg'
 
 const Home = () => {
     return (
         <div>
             <Nav />
             <section className="home__container">
-                <h1 className="home__title">My Shop</h1>
+                <h1 className="home__title">ECO-STORE</h1>
+                <p className="home__subtitle">The world's leading retailer of sustainably sourced products.</p>
+                <Link to="/products" className="home__button">SHOP NOW</Link>
+                <img src={World} alt="World"/>
             </section>
         </div>
     )
